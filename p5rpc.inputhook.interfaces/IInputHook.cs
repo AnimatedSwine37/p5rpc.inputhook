fd74cf64bcd87f1c7e7052fbc0bfefa81d8512c6
AnimatedSwine37 <24914353+AnimatedSwine37@users.noreply.github.com>
2022-11-12 14:49:27 +1000

    Add SendKey

--- p5rpc.inputhook.interfaces/IInputHook.cs
+++ p5rpc.inputhook.interfaces/IInputHook.cs
@@ -12,14 +12,24 @@ namespace p5rpc.inputhook.interfaces
         /// <summary>
         /// This event occurs whenever the game recieves an input just before it is processed
         /// With this event you only read the input, if you want to edit it use <see cref="OnInputIntercept"/>
+        /// This occurs after both <see cref="SendKey"/> and <see cref="OnInput"/>
         /// </summary>
         public event OnInputEvent OnInput;
 
         /// <summary>
         /// This event occurs whenever the game recieves an input just before it is processed
         /// With this event you can edit the input before it is processed, for just reading inputs use <see cref="OnInput"/>
+        /// This occurs before <see cref="OnInput"/> and after <see cref="SendKey"/>
         /// </summary>
         public event OnInputInterceptEvent OnInputIntercept;
+
+        /// <summary>
+        /// Queues a key to be sent to the game the next time it polls for inputs
+        /// This is added before the <see cref="OnInput"/> and <see cref="OnInputIntercept"/> events are run so it can be modified by other consumers
+        /// </summary>
+        /// <param name="key"></param>
+        public void SendKey(Key key);
+
     }
 
     /// <summary>
--- p5rpc.inputhook/InputHook.cs
+++ p5rpc.inputhook/InputHook.cs
@@ -26,7 +26,9 @@ namespace p5rpc.inputhook
 
         private IAsmHook _keyPressedHook;
 
-        private List<Key> _pressedKeys = new();
+        private List<Key> _keysToSend = new();
+
+        private bool _needToSendKeys = false;
 
         private IReverseWrapper<KeyPressedFunction> _keyPressedReverseWrapper;
 
@@ -41,7 +43,7 @@ namespace p5rpc.inputhook
             startupScanner.AddMainModuleScan("0F 28 74 24 ?? 48 8D 34 85 00 00 00 00", InitKeyboardHook);
         }
 
-        private void InitKeyboardHook(PatternScanResult result)
+        private unsafe void InitKeyboardHook(PatternScanResult result)
         {
             if (!result.Found)
             {
@@ -54,36 +56,52 @@ namespace p5rpc.inputhook
             string[] function =
             {
                 "use64",
-                // If no keys were pressed don't waste time calling the function
-                "cmp rax, 0",
-                "je endHook",
                 $"{_hooks.Utilities.GetAbsoluteCallMnemonics(KeyPressed, out _keyPressedReverseWrapper)}",
-                "label endHook",
             };
+            foreach (var line in function)
+            {
+                Utils.LogDebug(line);
+            }
             _keyPressedHook = _hooks.CreateAsmHook(function, result.Offset + Utils.BaseAddress, AsmHookBehaviour.ExecuteFirst).Activate();
         }
 
-        private int KeyPressed(int numKeys, nuint pressedKeyInfo)
+        private int KeyPressed(int numKeys, nuint pressedKeyInfo, bool needToSendKeys)
         {
+            if (numKeys == 0 && !_needToSendKeys)
+                return numKeys;
             FixedArrayPtr<Key> pressedKeys = new(pressedKeyInfo, numKeys);
             Utils.LogDebug($"Pressed keys: {string.Join(", ", pressedKeys)}");
             List<Key> pressedKeysList = pressedKeys.ToList();
-            if(OnInputIntercept?.GetInvocationList().Length != 0)
+
+            if (_needToSendKeys)
+            {
+                pressedKeysList.AddRange(_keysToSend);
+                _keysToSend.Clear();
+                _needToSendKeys = false;
+            }
+
+            if (OnInputIntercept?.GetInvocationList().Length != 0)
             {
                 OnInputIntercept?.Invoke(pressedKeysList);
                 _memory.Write(pressedKeyInfo, pressedKeysList.ToArray());
             }
 
             Task.Run(() => OnInput?.Invoke(pressedKeysList));
-            
+
             return pressedKeysList.Count;
+        }
+
+        public void SendKey(Key key)
+        {
+            _keysToSend.Add(key);
+            _needToSendKeys = true;
         }
 
         public event OnInputEvent OnInput;
 
         public event OnInputInterceptEvent OnInputIntercept;
 
-        [Function(new Register[] { Register.rax, Register.r13 }, Register.rax, false)]
-        private delegate int KeyPressedFunction(int numKeys, nuint pressedKeyInfo);
+        [Function(new Register[] { Register.rax, Register.r13, Register.rbx }, Register.rax, false)]
+        private delegate int KeyPressedFunction(int numKeys, nuint pressedKeyInfo, bool needToSendKeys);
     }
 }
